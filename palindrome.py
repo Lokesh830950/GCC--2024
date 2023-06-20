70362fb0f3d4e4ce49566195a5c4367561271714
Lokesh830950 <94075837+Lokesh830950@users.noreply.github.com>
2023-06-20 11:28:00 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,15 @@
+def pla(n):
+  flag=True
+  for i in range(len(n)):
+    if n[i]==n[len(n)-1-i]:
+      flag=True
+    else:
+      flag=False
+      break
+  if flag == True:
+    print("True")
+  else:
+    print("Flase")
+n=input()
+pla(n)
+
